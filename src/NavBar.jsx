7a930b6a2d5801821163bda7b3d3da6189590d30
Charles Goh <charlesgoh@Charless-MacBook-Pro.local>
2017-06-01 01:22:42 +0800

    [SRC][UI][NAV] Simplified navigation UI and added log in button

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,19 +1,24 @@
 import React, {Component} from 'react';
-import SideNav from './SideNav.jsx';
 
 export default class NavBar extends Component {
   render() {
     return (
       <nav>
         <div className="nav-wrapper red darken-4">
-
-          <a href="#!" className="brand-logo center">{ this.props.title }</a>
-
-          <SideNav />
-          <a href="#" data-activates="slide-out" className="button-collapse">
+          <a href="#!" className="brand-logo center">LevelUP</a>
+          <a href="#" data-activates="mobile-demo" className="button-collapse">
             <i className="material-icons">menu</i>
           </a>
-
+          <ul className="right hide-on-med-and-down">
+            <li>
+              <a className="waves-effect waves-light btn-large"><i className="large material-icons left">perm_identity</i>LOG IN</a>
+            </li>
+          </ul>
+          <ul className="side-nav" id="mobile-demo">
+            <li>
+              <a className="waves-effect waves-light btn-large"><i className="large material-icons left">perm_identity</i>LOG IN</a>
+            </li>
+          </ul>
         </div>
       </nav>
 
