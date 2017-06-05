23f4b8c0bdd4792b31468ab9292515ceaf959d52
Charles Goh <charlesgoh@Charless-MacBook-Pro.local>
2017-06-05 18:41:39 +0800

    [SRC][AUTH] Deleted redundant binding statements from previous commit

--- src/App.js
+++ src/App.js
@@ -23,9 +23,6 @@ export default class App extends Component {
     };
 
     this.login = this.login.bind(this);
-    this.logout = this.logout.bind(this);
-    this.handleAuthentication = this.handleAuthentication.bind(this);
-    this.isAuthenticated = this.isAuthenticated.bind(this);
   }
 
   componentWillMount() {
