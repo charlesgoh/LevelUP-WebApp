2af25e1a117ca90ffe421d356eb27ea952b13543
Charles Goh <charlesgoh@Charless-MacBook-Pro.local>
2017-06-07 14:58:23 +0800

    [SRC][ROUTER] Set up main structure of router using Route and BrowserRouter

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,23 +1,25 @@
 import React, { Component } from 'react';
-import ReactDOM, { render } from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 
 import App from './App';
 import Homepage from './Homepage/Homepage';
 import CategoryPage from './CategoryPage/CategoryPage';
+import NotFound from './NotFound';
 
 export default class Routes extends Component {
   render() {
-    <BrowserRouter>
-      <Route exact path="/" component={ App }>
-        <Route path="home" component= { Homepage }/>
-        <Route path="/categories" component={ CategoryPage }/>
-        {/* <Route path="users" component={ Users }>
-          <Route path="/users/:userID" component={ Users } />
-        </Route> */}
-        <Route path="*" component={ Null } />
-      </Route>
-    </BrowserRouter>
+    return (
+      <BrowserRouter>
+        <Route exact path="/" component={ App }>
+          <Route path="index" component= { Homepage }/>
+          <Route path="categories" component={ CategoryPage }/>
+          {/* <Route path="users" component={ Users }>
+            <Route path="/users/:userID" component={ Users } />
+          </Route> */}
+          <Route path="*" component={ NotFound } />
+        </Route>
+      </BrowserRouter>
+    )
   }
 }
