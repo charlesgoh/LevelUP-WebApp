05d237ded982a90724e5fe0c2bbe994bdcf8e236
Charles Goh <charlesgoh@Charless-MacBook-Pro.local>
2017-06-09 11:10:00 +0800

    [SRC][AUTH] Created modal layout

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -5,12 +5,25 @@ export default class NavBar extends Component {
   render() {
     let authButton;
     authButton = (
-      <a className="waves-effect waves-light btn">
-        <i className="material-icons left">perm_identity</i>LOG IN BUTTON</a>
-    )
+      <div>
+          <a className="waves-effect waves-light btn" href="#modal1">
+            <i className="material-icons left">perm_identity</i>LOGIN
+          </a>
+      </div>
+    );
 
     return (
       <nav>
+        {/* This is the login Modal */}
+        <div id="modal1" className="modal">
+          <div className="center modal-content red darken-4">
+            <h1>LevelUP</h1>
+          </div>
+          <div className="modal-footer">
+            <a href="#!" className="modal-action modal-close waves-effect waves-green btn-flat">Agree</a>
+          </div>
+        </div>
+
         <div className="nav-wrapper red darken-4">
           <a href="#" className="brand-logo center">LevelUP</a>
           <a href="#" data-activates="mobile-demo" className="button-collapse">
