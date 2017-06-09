64e0a76eba0c5a0a9196babe805ab172b5d32480
Charles Goh <charlesgoh@Charless-MacBook-Pro.local>
2017-06-09 08:53:20 +0800

    [APP] Transfered dependency burden to external sources. Uninstalled packages from server environment

--- src/App.js
+++ src/App.js
@@ -3,17 +3,6 @@ import NavBar from './NavBar';
 import Homepage from './Homepage/Homepage';
 import firebase from 'firebase';
 
-// Initialize Firebase
- var config = {
-   apiKey: "AIzaSyCNY41AjwQcTN2bRSc76jd9biY67j5HKEo",
-   authDomain: "levelup-30839.firebaseapp.com",
-   databaseURL: "https://levelup-30839.firebaseio.com",
-   projectId: "levelup-30839",
-   storageBucket: "levelup-30839.appspot.com",
-   messagingSenderId: "109258915713"
- };
- firebase.initializeApp(config);
-
 export default class App extends Component {
 
   constructor(props) {
