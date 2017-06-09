270d3483766b5267aae7709669bb612c736aa353
Charles Goh <charlesgoh@Charless-MacBook-Pro.local>
2017-06-09 23:01:13 +0800

    [FIREBASE][CONFIG] Exported useful namespaces from App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
 import React, {Component} from 'react';
 import NavBar from './NavBar';
 import Homepage from './Homepage/Homepage';
+import firebase from 'firebase';
+
+let config = {
+  apiKey: "AIzaSyCNY41AjwQcTN2bRSc76jd9biY67j5HKEo",
+  authDomain: "levelup-30839.firebaseapp.com",
+  databaseURL: "https://levelup-30839.firebaseio.com",
+  projectId: "levelup-30839",
+  storageBucket: "levelup-30839.appspot.com",
+  messagingSenderId: "109258915713"
+}
+
+export const firebaseApp = firebase.initializeApp(config);
+
+export const db = firebaseApp.database();
+export const auth = firebaseApp.auth();
+
+export const storageKey = 'KEY_FOR_LOCAL_STORAGE';
 
 export default class App extends Component {
 
