645a0b28b7d92159edb77fd4b5d29159f9eeacb4
Charles Goh <charlesgoh@users.noreply.github.com>
2017-06-13 15:35:40 +0800

    Revert "[REVERT] To prevent deletion of new components"

--- src/CategoryPage/ProfileBar.jsx
+++ src/CategoryPage/ProfileBar.jsx
@@ -1,40 +1,35 @@
 import React, { Component }from 'react';
-import firebase from 'firebase';
 
 export default class ProfileBar extends Component {
   render() {
     return (
-      <div className = 'card-panel z-depth-1'>
-        <div className = 'container center-align grey lighten-2'>
-          <div className = 'row valign-wrapper'>
-            <div className = 'col s2'>
-              <i className = 'small material-icons'>
-                info_outline
-              </i>
-            </div>
-
-            <div className = 'col s7 left-align'>
-              <p className = 'flow-text grey-text'>
-                {firebase.auth().currentUser.displayName}
-              </p>
-
-              <p className = 'flow-text'>
-                <a href = '#' className = 'red-text text-darken-4'>
-                  Profile
-                </a>
-              </p>
-            </div>
-
-            <div className = 'col s3 grey-text center-align'>
-              <img src = "./images/Logout.png" className = 'responsive-image' />
-              <p>
-                <a href ='#' className = 'flow-text grey-text'>
-                  LOG OUT
-                </a>
-              </p>
-            </div>
-
+      <div className = 'container center-align grey lighten-2'>
+        <div className = 'row valign-wrapper'>
+          <div className = 'col s2'>
+            <i className = 'small material-icons'>
+              info_outline
+            </i>
           </div>
+
+          <div className = 'col s7 left-align'>
+            <p className = 'flow-text grey-text'>
+              Name
+            </p>
+
+            <p className = 'flow-text'>
+              <a href = '#' className = 'red-text text-darken-4'>
+                Profile
+              </a>
+            </p>
+          </div>
+
+          <div className = 'col s3 grey-text center-align'>
+            <img src="./images/Logout.png" className = 'responsive-image' />
+            <p className = 'flow-text grey-text'>
+              LOG OUT
+            </p>
+          </div>
+
         </div>
       </div>
     );
