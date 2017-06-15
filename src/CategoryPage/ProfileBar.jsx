41495f0c111e522b787891819cdf49f91d2067db
Charles Goh <charlesgoh@Charless-MacBook-Pro.local>
2017-06-15 20:06:05 +0800

    [SRC][PROFILEBAR] Fixed className syntax issue

--- src/CategoryPage/ProfileBar.jsx
+++ src/CategoryPage/ProfileBar.jsx
@@ -4,7 +4,7 @@ import firebase from 'firebase';
 export default class ProfileBar extends Component {
   render() {
     return (
-      <div class="col s12 m8 offset-m2 l6 offset-l3">
+      <div className="col s12 m8 offset-m2 l6 offset-l3">
         <div className = 'card-panel z-depth-1'>
           <div className = 'center-align'>
             <div className = 'row valign-wrapper'>
