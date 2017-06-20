99667fc7bbaca699767cb583131578289eed0906
Charles Goh <charlesgoh@Charless-MacBook-Pro.local>
2017-06-20 10:55:22 +0800

    [CRUD][PROFILE] Successfully isolated description using window.localStorage and firebasedb

--- src/ProfilePage/UserProfile.jsx
+++ src/ProfilePage/UserProfile.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import firebase from 'firebase';
-
+import * as FirebaseService from '../FirebaseService.jsx';
 
 
 export default class ProfilePage extends Component {
@@ -25,23 +25,42 @@ export default class ProfilePage extends Component {
   }
 
   handleChange(event) {
+    var user = firebase.auth().currentUser;
     this.setState({description: event.target.value});
+
+    firebase.database().ref('users/' + user.uid).update({
+      description: event.target.value
+    });
   }
 
   handleSubmit(event) {
     event.preventDefault();
   }
   render () {
-
+    console.log("Entered render part");
     var clickable = {
       cursor: "pointer"
     };
 
     var user = firebase.auth().currentUser;
+    console.log("User is now: " + user);
     var name = '';
     var photoUrl = '';
     var description = "";
+
+    var uid = window.localStorage.getItem(FirebaseService.storageKey);
+    console.log("storageKey is: " + uid);
+    firebase.database().ref().on("value", function(snapshot) {
+      var thisUser = snapshot.val();
+      console.log(thisUser["users"][uid]["description"]);
+      description = thisUser["users"][uid]["description"];
+    }, function (error) {
+      console.log("Error: " + error.code);
+    });
+
     if (user){
+      console.log("User is " + user);
+      this.state.description = user.description;
       name = user.displayName;
       photoUrl = user.photoURL;
       if (!this.state.description){
