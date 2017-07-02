1f6b0ebfaf588baf662046a1437bd0a382368cac
Charles Goh <charlesgoh@Charless-MacBook-Pro.local>
2017-07-02 22:20:55 +0800

    [PROFILE-BAR-NAV] Fixed link and compile warning

--- src/CategoryPage/ProfileBar.jsx
+++ src/CategoryPage/ProfileBar.jsx
@@ -1,21 +1,29 @@
 import React, { Component }from 'react';
 import firebase from 'firebase';
+import { Link } from 'react-router-dom';
 
 export default class ProfileBar extends Component {
+
   render() {
+    var user = firebase.auth().currentUser;
     return (
       <div className="col s12 m8 offset-m2 l6 offset-l3">
         <div className = 'card-panel z-depth-1'>
           <div className = 'center-align'>
             <div className = 'row valign-wrapper'>
               <div className = 'col s4'>
-                <img className = "circle responsive-img" src={firebase.auth().currentUser.photoURL} />
+                <img alt="" className="circle responsive-img" src={user.photoURL} />
               </div>
 
               <div className = 'col s8 left-align'>
-                <a href="#" className = 'flow-text grey-text text-darken-2'>
-                    {firebase.auth().currentUser.displayName}
-                </a>
+                <Link to={{
+                  pathname: "/profile/id?=" + user.uid,
+                  state: {
+                    uid: user.uid
+                  }}
+                }>
+                  <strong className="flow-text black-text">{user.displayName}</strong>
+                </Link>
               </div>
             </div>
           </div>
