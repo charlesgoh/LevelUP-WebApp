f5df770731127580be01caf04aea0831c39ed4d8
Esilocke <klin_zw@hotmail.com>
2017-07-23 11:07:23 +0800

    [UI] Implemented overflow control into the project. Users can now no longer break the UI by inputting long words into fields. Also added validity checking for negative numbers into pricing.

--- src/CategoryPage/ProfileBar.jsx
+++ src/CategoryPage/ProfileBar.jsx
@@ -2,6 +2,7 @@ import React, { Component }from 'react';
 import firebase from 'firebase';
 import { Link } from 'react-router-dom';
 import { Row, Col, CardPanel } from 'react-materialize';
+import styles from '../GlobalStyles.css';
 
 export default class ProfileBar extends Component {
 
@@ -16,7 +17,7 @@ export default class ProfileBar extends Component {
 
           <Col s={8} className='center-align'>
             <a href={"/profile/id?=" + user.uid}>
-              <strong className="flow-text black-text ">{user.displayName}</strong>
+              <strong className="flow-text black-text overflow-control">{user.displayName}</strong>
             </a>
           </Col>
         </Row>
