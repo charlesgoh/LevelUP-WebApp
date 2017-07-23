bdb2c95ac974476f16f0f2b925f1ee6537fff5e1
Charles Goh <charlesgoh@118-Infinite-Revelation.local>
2017-07-23 19:57:27 +0800

    [NAV][ICONS] Changed category tag icon to accessibility

--- src/CategoryPage/SortFilter.jsx
+++ src/CategoryPage/SortFilter.jsx
@@ -58,7 +58,7 @@ export default class SortFilter extends Component {
           <Row className='row black-text'>
             <Col s={2}>
               <Icon>
-                room
+                accessibility
               </Icon>
             </Col>
             <Col s={10}>
