fca55ab0fadce4e81f4c3df351384d37056b3db5
Esilocke <klin_zw@hotmail.com>
2017-07-27 17:58:35 +0800

    [UI] Changed the UI of ProfilePage to a multi-tab system, in preparation for multiple listings.

--- src/ProfilePage/ProfilePage.jsx
+++ src/ProfilePage/ProfilePage.jsx
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
 import UserProfile from './UserProfile';
-import Listing from './Listing';
+import ListingsPage from './ListingsPage.jsx';
+import ReviewPage from './ReviewPage';
+import { Modal, Button, CardPanel, Row, Col, Tabs, Tab } from 'react-materialize';
 
 export default class ProfilePage extends Component {
   render (){
     let uidProp = window.location.search.slice(2);
+
     return (
       <div>
-        <UserProfile uid={uidProp}/>
-        <Listing uid={uidProp} />
+        <Tabs className='tab-demo z-depth-1'>
+          <Tab title="About Me" active>
+            <UserProfile uid={uidProp} />
+          </Tab>
+          <Tab title="Listings">
+            <ListingsPage uid={uidProp} />
+          </Tab>
+          <Tab title="Reviews">
+            <ReviewPage uid={uidProp} />
+          </Tab>
+        </Tabs>
       </div>
     );
   }
