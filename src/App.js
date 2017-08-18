413ce81f0a3cb0515c24987b98abfafd5c31cc16
Charles Goh <charlesgoh@118-Infinite-Revelation.local>
2017-08-18 20:03:39 +0800

    [SLIDER] Added motivational quotes as placeholders :)

--- src/App.js
+++ src/App.js
@@ -116,20 +116,14 @@ export default class App extends Component {
         <Slide src={require("./Graphics/Slider/Slider0.png")} title="No Registration Required" placement="right">
           Sign in with Google
         </Slide>
-        <Slide src="https://lorempixel.com/800/400/sports/1" title="Tagline">
-          Slogan for slide 1
+        <Slide src="https://lorempixel.com/800/400/sports/1" title="The secret of getting ahead is getting started" placement="right">
+          Mark Twain
         </Slide>
-        <Slide src="https://lorempixel.com/800/400/sports/2" title="Tagline" placement="left">
-          Slogan for slide 2
+        <Slide src="https://lorempixel.com/800/400/sports/2" title="The will to win, the desire to succeed, the urge to reach your full potential... these are the keys that will unlock the door to personal excellence" placement="right">
+          Confucius
         </Slide>
-        <Slide src="https://lorempixel.com/800/400/sports/3" title="Tagline" placement="right">
-          Slogan for slide 3
-        </Slide>
-        <Slide src="https://lorempixel.com/800/400/sports/6" title="Tagline" placement="left">
-          Slogan for slide 4
-        </Slide>
-        <Slide src="https://lorempixel.com/800/400/sports/8" title="Tagline" placement="right">
-          Slogan for slide 5
+        <Slide src="https://lorempixel.com/800/400/sports/8" title="As you think, so you shall become" placement="right">
+          Bruce Lee
         </Slide>
       </Slider>
     );
