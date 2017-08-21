dac297e092e6bd383d54209949776d7eea76802d
Charles Goh <charlesgoh@r-239-99-25-172.comp.nus.edu.sg>
2017-08-21 14:18:00 +0800

    [FIX] Used separate https secure host flickr to host slider images

--- src/App.js
+++ src/App.js
@@ -113,16 +113,16 @@ export default class App extends Component {
   render() {
     var slider = (
       <Slider>
-        <Slide src={require("./Graphics/Slider/Slider 1.png")} title="No Registration Required" placement="right">
+        <Slide src="https://farm5.staticflickr.com/4436/35871686044_70c4e90578_b.jpg" title="No Registration Required" placement="right">
           Sign in with Google
         </Slide>
-        <Slide src={require("./Graphics/Slider/Slider 2.png")} title="The secret of getting ahead is getting started" placement="right">
+        <Slide src="https://farm5.staticflickr.com/4337/36311327310_4d297ea379_b.jpg" title="The secret of getting ahead is getting started" placement="right">
           Mark Twain
         </Slide>
-        <Slide src={require("./Graphics/Slider/Slider 3.png")} title="The will to win, the desire to succeed, the urge to reach your full potential... these are the keys that will unlock the door to personal excellence" placement="right">
+        <Slide src="https://farm5.staticflickr.com/4370/36706542465_8aa13a24e6_b.jpg" title="The will to win, the desire to succeed, the urge to reach your full potential... these are the keys that will unlock the door to personal excellence" placement="right">
           Confucius
         </Slide>
-        <Slide src={require("./Graphics/Slider/Slider 4.png")} title="As you think, so you shall become" placement="right">
+        <Slide src="https://farm5.staticflickr.com/4358/36311327000_c9040dc97f_b.jpg" title="As you think, so you shall become" placement="right">
           Bruce Lee
         </Slide>
       </Slider>
