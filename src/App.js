f3a9d05b2973fc621b78a76a3f915a0fe117d63f
Charles Goh <charlesgoh@r-239-99-25-172.comp.nus.edu.sg>
2017-08-21 14:21:50 +0800

    [SLIDER][QUOTE] Changed quote from confucious to one from JFK

--- src/App.js
+++ src/App.js
@@ -119,8 +119,8 @@ export default class App extends Component {
         <Slide src="https://farm5.staticflickr.com/4337/36311327310_4d297ea379_b.jpg" title="The secret of getting ahead is getting started" placement="right">
           Mark Twain
         </Slide>
-        <Slide src="https://farm5.staticflickr.com/4370/36706542465_8aa13a24e6_b.jpg" title="The will to win, the desire to succeed, the urge to reach your full potential... these are the keys that will unlock the door to personal excellence" placement="right">
-          Confucius
+        <Slide src="https://farm5.staticflickr.com/4370/36706542465_8aa13a24e6_b.jpg" title="Physical fitness is not only one of the most important keys to a healthy body, it is the basis of dynamic and creative intellectual activity" placement="right">
+          John F. Kennedy
         </Slide>
         <Slide src="https://farm5.staticflickr.com/4358/36311327000_c9040dc97f_b.jpg" title="As you think, so you shall become" placement="right">
           Bruce Lee
