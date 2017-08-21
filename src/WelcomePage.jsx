af0e8fe200d5a0dadacaca1121644e83d33dee39
Charles Goh <charlesgoh@r-239-99-25-172.comp.nus.edu.sg>
2017-08-21 13:32:55 +0800

    [FIX][SLIDER] Changed source of images to local source to avoid SSL Certificate problems

--- src/WelcomePage.jsx
+++ src/WelcomePage.jsx
@@ -5,16 +5,16 @@ export default class WelcomePage extends Component {
   render() {
     return (
       <Slider>
-        <Slide src={require("./Graphics/Slider/Slider0.png")} title="No Registration Required" placement="right">
+        <Slide src={require("./Graphics/Slider/Slider 1.png")} title="No Registration Required" placement="right">
           Sign in with Google
         </Slide>
-        <Slide src="https://lorempixel.com/800/400/sports/1" title="The secret of getting ahead is getting started" placement="right">
+        <Slide src={require("./Graphics/Slider/Slider 2.png")} title="The secret of getting ahead is getting started" placement="right">
           Mark Twain
         </Slide>
-        <Slide src="https://lorempixel.com/800/400/sports/2" title="The will to win, the desire to succeed, the urge to reach your full potential... these are the keys that will unlock the door to personal excellence" placement="right">
+        <Slide src={require("./Graphics/Slider/Slider 3.png")} title="The will to win, the desire to succeed, the urge to reach your full potential... these are the keys that will unlock the door to personal excellence" placement="right">
           Confucius
         </Slide>
-        <Slide src="https://lorempixel.com/800/400/sports/8" title="As you think, so you shall become" placement="right">
+        <Slide src={require("./Graphics/Slider/Slider 4.png")} title="As you think, so you shall become" placement="right">
           Bruce Lee
         </Slide>
       </Slider>
